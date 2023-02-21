991609043c9ce8e7de912d0f65f1bfb40a1194ec
AnhTran <letrannhatanh171@gmail.com>
2023-02-21 14:41:00 +0700

    Add project files.

---
+++ BTH14_DKiHoc/resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by BTH14_DKiHoc.rc
+//
+#define IDC_MYICON                      2
+#define IDD_BTH14DKIHOC_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_BTH14DKIHOC                 107
+#define IDI_SMALL                       108
+#define IDC_BTH14DKIHOC                 109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define BTN_LAMLAI                      1000
+#define BTN_DKI                         1001
+#define BTN_THOAT                       1002
+#define BTN_XOA                         1003
+#define ID_EDIT_MA                      1004
+#define ID_COMBO_KHOA                   1005
+#define ID_EDIT_TEN                     1006
+#define ID_COMBO_kHOAS                  1007
+#define ID_CHECK_TINHOC                 1008
+#define ID_CHECK_GIAITICH               1009
+#define ID_CHECK_TRIETHOC               1010
+#define ID_CHECK_TIENGANH               1011
+#define IDC_LIST_DKH                    1012
+#define ID_LIST_DKH                     1012
+#define ID_CH32771                      32771
+#define ID_DKH                          32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
